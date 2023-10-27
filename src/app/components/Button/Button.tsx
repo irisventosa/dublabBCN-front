df606067f299a0ee1b859f980da65a96ca0b89a3
Raúl Pérez <jrpcassone@gmail.com>
2023-10-27 15:53:06 +0200

    Create button component  and volume bar

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithChildren } from "react";
+
+interface ButtonProps extends PropsWithChildren {
+  className?: string;
+  actionOnClick?: () => void;
+  disabled?: boolean;
+  type?: "button" | "submit" | "reset";
+}
+
+const Button = ({
+  className,
+  actionOnClick,
+  children,
+  disabled,
+  type,
+}: ButtonProps): React.ReactElement => {
+  const buttonClass = `button ${className}`;
+
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={buttonClass}
+      onClick={actionOnClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/app/components/RadioPlayer/RadioPlayer.tsx
+++ src/app/components/RadioPlayer/RadioPlayer.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from "react";
 import Image from "next/image";
+import Button from "../Button/Button";
 
 const RadioPlayer = (): ReactElement => {
   return (
-    <ul className="flex gap-[91px] justify-between items-center py-2 px-8 flex-row bg-black text-white font-Favorit text-sm	 font-s font-light uppercase ">
+    <ul className="flex gap-[91px] justify-between items-center py-2 px-8 flex-row bg-black text-white font-Favorit text-sm font-light uppercase ">
       <li>BARCELONA 10:11:00</li>
       <li className="flex gap-[9px]">
         <Image
@@ -15,12 +16,23 @@ const RadioPlayer = (): ReactElement => {
         En directe:
         <span className="ml-[19px]">The Sinners Inn Show - ARTIST NAME</span>
       </li>
-      <li className="">
-        PRÒXIM:{" "}
+      <li className="text-white/60">
+        PRÒXIM:
         <span className="ml-[19px]">10:00 Persona (R) - ARTIST NAME</span>{" "}
       </li>
-      <li className="w-40">VOLUME</li>
-      <li>PAUSE</li>
+      <li>
+        <span className="w-40 hover:invisible">VOLUME</span>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          step="1"
+          className=" w-40transparent h-4 w-full appearance-none border-transparent bg-zinc-500 accent-white  "
+        />
+      </li>
+      <li>
+        <Button>PAUSE</Button>
+      </li>
     </ul>
   );
 };
