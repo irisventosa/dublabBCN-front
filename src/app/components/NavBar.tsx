bdcda37a02a4b7fe61f11918aa2938b9c2f9852f
Raúl Pérez <jrpcassone@gmail.com>
2023-11-06 16:06:15 +0100

    Fix styles

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,8 +12,9 @@ const links = [
 ];
 
 const NavBar = (): React.ReactElement => {
+  //el margin left con el diseño que esta hecho para resolucion macboock14 se sale del vw cuando la res es menor
   return (
-    <nav className=" bg-[#ECECEC] w-[719px] h-[42px] ml-[101.74px] flex justify-center rounded-md	font-Favorit text-sm font-normal uppercase  ">
+    <nav className=" bg-[#ECECEC] min-w-[719px] h-[42px] ml-[21.74px] flex justify-center rounded-md font-Favorit text-sm font-normal uppercase  ">
       <ul className=" flex items-center flex-row justify-between gap-[49px] ">
         {links.map(({ label, route }) => (
           <li key={route}>
