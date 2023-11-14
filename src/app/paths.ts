d43efa106b987e0ba9131485222f348555c208b2
Raúl Pérez <jrpcassone@gmail.com>
2023-11-14 11:57:04 +0100

    Fix Show card props and styles

---
+++ src/app/paths.ts
@@ -0,0 +1,16 @@
+export const linksStart = [
+  { label: "Latest", route: "/latest" },
+  { label: "Calendari", route: "/calendari" },
+  { label: "Arxiu", route: "/arxiu" },
+];
+
+export const linksEnd = [
+  { label: "Sisters", route: "/sisters" },
+  { label: "Merch", route: "/merch" },
+  { label: "Info", route: "/info" },
+];
+
+export const programesDropDown = [
+  { label: "shows", route: "/shows" },
+  { label: "b-sides", route: "/b-sides" },
+];
