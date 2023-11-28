e85dc067289dc4ff942b48c2b450ef582b00ec5a
Raúl Pérez <jrpcassone@gmail.com>
2023-11-28 15:27:37 +0100

    Refactor variable height list

--- src/app/latest/page.tsx
+++ src/app/latest/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LatestShowsVariableHeight from "../components/LatestShows/LatestShowsVariableHeight";
+import { getLatestShowsData } from "../lib/radioDataFetching";
 
-const Latest = (): React.ReactElement => {
+const Latest = async () => {
+  const { results } = await getLatestShowsData();
   return (
     <main className="flex flex-col">
-      <LatestShowsVariableHeight />
+      <LatestShowsVariableHeight latestShows={results} />
     </main>
   );
 };
