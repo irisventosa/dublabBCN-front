a48290c52eecaabdefbd7c160221ae06257ada62
Raúl Pérez <jrpcassone@gmail.com>
2023-12-01 12:59:32 +0100

    Create Services and thanksto components

---
+++ src/app/components/Info/ServicesAndClients.tsx
@@ -0,0 +1,15 @@
+const ServicesAndClients = () => {
+  return (
+    <div className="h-[194px] w-[810px] relative left-[490px] pt-[88px]">
+      <p>
+        A més de gestió de continguts, organitzem concerts, esdeveniments,
+        projeccions i xerrades com Sònar, CCCB, HUB, MUTEK, Convent de Sant
+        Agustí, Arts Libris, Zumzeig, Miscelanea, Flea Market Bcn,
+        L&apos;Alternativa, FAD, Festival Sâlmon, Independent Label Market,
+        Ameba Barcelona, tupper o Palo alto Market.
+      </p>
+    </div>
+  );
+};
+
+export default ServicesAndClients;
