c13c7b39c341a29d515828d5632b6488f6d83773
Raúl Pérez <jrpcassone@gmail.com>
2023-12-10 18:27:25 +0100

    Correct type in showcard

--- src/app/lib/hooks/useAirtimeApi.ts
+++ src/app/lib/hooks/useAirtimeApi.ts
@@ -1,19 +1,32 @@
 import { LiveRadioData, WeekInfo } from "@/app/types";
 import axios from "axios";
+import { useCallback } from "react";
 
 const streamingData = "https://dublabbcn.airtime.pro/api/live-info";
 const weekInfo = "https://dublabbcn.airtime.pro/api/week-info";
 
 const useAirtimeApi = () => {
-  const getWeekInfo = async () => {
-    const { data: schedule } = await axios.get<WeekInfo>(weekInfo);
-    return schedule;
-  };
+  const getWeekInfo = useCallback(async () => {
+    try {
+      const { data: schedule } = await axios.get<WeekInfo>(weekInfo);
+      return schedule;
+    } catch (error) {
+      const message = "Error al conectar a la API de airtime";
+      throw new Error(message);
+    }
+  }, []);
 
-  const getLiveRadioData = async () => {
-    const { data: onAirRadio } = await axios.get<LiveRadioData>(streamingData);
-    return onAirRadio;
-  };
+  const getLiveRadioData = useCallback(async () => {
+    try {
+      const { data: onAirRadio } = await axios.get<LiveRadioData>(
+        streamingData
+      );
+      return onAirRadio;
+    } catch (error) {
+      const message = "Error al conectar a la API de airtime";
+      throw new Error(message);
+    }
+  }, []);
 
   return { getWeekInfo, getLiveRadioData };
 };
