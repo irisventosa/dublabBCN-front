f8253153ab7411a018229315ef6045382b2ed06a
Raúl Pérez <jrpcassone@gmail.com>
2024-01-07 23:41:11 +0100

    Correct show cards padding in shows page and correct styles

--- src/app/components/LatestShows/LatestShowsVariableHeight.tsx
+++ src/app/components/LatestShows/LatestShowsVariableHeight.tsx
@@ -28,7 +28,9 @@ const LatestShowsVariableHeight = ({
 
   return (
     <section className="py-[65px] px-[31px]">
-      <ul className={`grid grid-cols-4 gap-x-3 gap-y-14 pt-[${paddingTop}]`}>
+      <ul
+        className={`grid grid-cols-4 grid-flow-row-dense gap-x-3 gap-y-14 pt-[${paddingTop}]`}
+      >
         {latestPodcasts.map((show) => (
           <li
             key={show.slug}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@ import HeroSection from "./components/HeroSection";
 import LatestShowsFixedHeight from "./components/LatestShows/LatestShowsFixedHeight";
 import useDublabApi from "./lib/hooks/useDublabApi";
 import mergeBsidesWithShows from "./lib/mergeBsidesWithShows";
-import { getLatestShowsData } from "./lib/radioDataFetching";
 
 const Home = async () => {
   const pageToGet = 1;
-  const { getBsides } = useDublabApi();
+  const { getBsides, getLatestShowsData } = useDublabApi();
 
   const { results: latestShows } = await getLatestShowsData(pageToGet);
   const { results: bSides } = await getBsides(pageToGet);
