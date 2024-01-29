babb155ae5d46e6f5ab39bd4a5ab9eb6fa12cf00
Raúl Pérez <jrpcassone@gmail.com>
2024-01-29 17:52:39 +0100

    Correct b-side show metadata title

--- src/app/b-sides/[slug]/page.tsx
+++ src/app/b-sides/[slug]/page.tsx
@@ -1,12 +1,8 @@
-"use client";
-import Button from "@/app/components/Button";
-import Tracklist from "@/app/components/Tracklist";
+import BsideInfo from "@/app/components/BsideDetailedContent";
 import extractUrlForEmbedPlayer from "@/app/lib/extractUrlForEmbedPlayer";
 import useDublabApi from "@/app/lib/hooks/useDublabApi";
-import { Bside } from "@/app/types";
+import { Metadata } from "next";
 import Image from "next/image";
-import { useState } from "react";
-import useSWR from "swr";
 
 interface BSideDetailsProps {
   params: {
@@ -14,16 +10,32 @@ interface BSideDetailsProps {
   };
 }
 
-const BsideDetails = ({ params }: BSideDetailsProps) => {
-  const [iFrameShow, setIFrameShow] = useState("");
+export const generateMetadata = ({ params }: BSideDetailsProps): Metadata => {
+  const transformFirstLetter = (firstLetter: string) => {
+    return firstLetter.toUpperCase();
+  };
+
+  const capitalizeWords = (showName: string) => {
+    const formatedShowName = showName
+      .replace(/\b\w/g, transformFirstLetter)
+      .replace("-", " ");
+
+    return formatedShowName;
+  };
+
+  const slug = capitalizeWords(params.slug.replace("-", " "));
+
+  return {
+    title: `${slug} | dublab`,
+    description: `Escolta l'arxiu del en directe del programa ${slug}`,
+  };
+};
+
+const BsideDetails = async ({ params }: BSideDetailsProps) => {
   const { getBsideData } = useDublabApi();
-  const { data: bside } = useSWR<Bside>(params.slug, getBsideData);
+  const bside = await getBsideData(params.slug);
 
   if (!bside) return <div>Loading...</div>;
-
-  const listenToBside = (bsideLink: string) => {
-    setIFrameShow(bsideLink);
-  };
 
   const showUrl = extractUrlForEmbedPlayer(bside.mixcloud_url);
   const description = {
@@ -38,53 +50,14 @@ const BsideDetails = ({ params }: BSideDetailsProps) => {
         width={660}
         height={327}
         className="h-[727px] object-cover"
-      ></Image>
-      <section className="max-h-[750px] min-w-[720px] overflow-scroll scrollbar-hide  bg-black">
-        <div className="flex justify-between items-end">
-          <ul className="flex">
-            <li>
-              <Button
-                className="uppercase"
-                actionOnClick={() => listenToBside(showUrl)}
-              >
-                Listen
-              </Button>
-            </li>
-            <span className="loader"></span>
-          </ul>
-          {bside.tags && (
-            <ul className="flex gap-[10px] opacity-40 mr-8">
-              {bside.tags.map((tag, index) => (
-                <li
-                  key={index}
-                  className=" text-[11px] border rounded-md pt-[5px] px-2 pb-[3px]"
-                >
-                  {tag}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <h2 className="text-5xl h-[58px] mt-[56px]">{bside.name}</h2>
-        <section className="flex gap-[140px]">
-          <div
-            dangerouslySetInnerHTML={description}
-            className="text-sm w-[445px] mt-[140px]"
-          />
-        </section>
-        <section className="flex-col items-end">
-          {bside.tracklist && <Tracklist tracklist={bside.tracklist} />}
-        </section>
-      </section>
-      {iFrameShow && (
-        <iframe
-          title="Programa de radio seleccionat"
-          className="sm:w-screen w-screen  fixed bottom-0 left-0"
-          height="60"
-          allow="autoplay"
-          src={`https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&autoplay=1&feed=/${iFrameShow}`}
-        ></iframe>
-      )}
+      />
+      <BsideInfo
+        showUrl={showUrl}
+        description={description}
+        name={bside.name}
+        tags={bside.tags}
+        tracklist={bside.tracklist}
+      />
     </main>
   );
 };
