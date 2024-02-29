4904fe70eeb26607a7584b7d192ac6bbad9c9031
Raúl Pérez <jrpcassone@gmail.com>
2024-02-29 10:35:24 +0100

    Correct width and spacings

--- src/app/components/Header/NavBar.tsx
+++ src/app/components/Header/NavBar.tsx
@@ -14,14 +14,14 @@ const NavBar = (): React.ReactElement => {
           <nav className="hidden sm:visible bg-[#ECECEC] px-12 h-[42px] sm:flex justify-center rounded-md font-Favorit text-sm font-normal uppercase">
             <ul className="flex items-center flex-row justify-between gap-10 lg:gap-[80px]">
               {firstNavLabels.map(({ label, route }) => (
-                <li key={route} className="pt-[2px]">
+                <li key={route} className="pt-[2px] hover:opacity-50">
                   <Link href={route}>{label}</Link>
                 </li>
               ))}
               <Programes />
               <Sisters />
               {lastNavLabels.map(({ label, route }) => (
-                <li key={route} className="pt-[2px]">
+                <li key={route} className="pt-[2px] hover:opacity-50">
                   <Link href={route}>{label}</Link>
                 </li>
               ))}
--- src/app/components/Tracklist.tsx
+++ src/app/components/Tracklist.tsx
@@ -16,7 +16,7 @@ const Tracklist = ({ tracklist }: TracklistProps) => {
       <section className="mt-14">
         <h2>Tracklist</h2>
         <hr
-          className={`border-${lineColor} sm:w-[719px] w-[98%] pr-4 p-[2px] `}
+          className={`border-${lineColor} sm:w-[619px] w-[98%] pr-4 p-[2px] `}
         />
         <span className="text-[14px]">
           El tracklist no esta disponible en aquest moment
@@ -30,7 +30,7 @@ const Tracklist = ({ tracklist }: TracklistProps) => {
   return (
     <section className=" w-fit lg:min-w-[1000px] mt-7 sm:mt-14">
       <h2>Tracklist</h2>
-      <hr className={`border-${lineColor} sm:w-[719px] w-[98%] pr-4 p-1 `} />
+      <hr className={`border-${lineColor} sm:w-[619px] w-[98%] pr-4 p-1 `} />
       <ul className="text-[14px]  mb-4 ">
         {formattedTracklist.map((track, index) => (
           <li key={index}>{`${(index + 1)
