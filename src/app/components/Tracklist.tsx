4c3338cc6b39703c98671e9c4a0712404aef30aa
Raúl Pérez <jrpcassone@gmail.com>
2024-02-29 22:14:58 +0100

    Correct boxes and tracklist line

--- src/app/components/Tracklist.tsx
+++ src/app/components/Tracklist.tsx
@@ -15,9 +15,7 @@ const Tracklist = ({ tracklist }: TracklistProps) => {
     return (
       <section className="mt-14">
         <h2>Tracklist</h2>
-        <hr
-          className={`border-${lineColor} sm:w-[619px] w-[98%] pr-4 p-[2px] `}
-        />
+        <hr className={`border-${lineColor} sm:w-auto w-[98%] pr-4 p-[2px] `} />
         <span className="text-[14px]">
           El tracklist no esta disponible en aquest moment
         </span>
@@ -30,7 +28,7 @@ const Tracklist = ({ tracklist }: TracklistProps) => {
   return (
     <section className=" w-fit lg:min-w-[1000px] mt-7 sm:mt-14">
       <h2>Tracklist</h2>
-      <hr className={`border-${lineColor} sm:w-[619px] w-[98%] pr-4 p-1 `} />
+      <hr className={`border-${lineColor} sm:w-auto w-[98%] pr-4 p-1 `} />
       <ul className="text-[14px]  mb-4 ">
         {formattedTracklist.map((track, index) => (
           <li key={index}>{`${(index + 1)
